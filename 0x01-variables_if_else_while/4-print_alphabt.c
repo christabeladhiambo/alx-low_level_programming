30f2ff97279f1a20171962e0e68cb57025792607
christabel <christabeladhiambo312gmail.com>
2022-03-11 12:41:44 -0800

    task 6

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,8 @@ int main(void)
 	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
-        	{
-			putchar(ch);
-		}
+		{
+		        putchar(ch);											     }
 		ch++;
 	}
 	putchar('\n');
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0,
- * followed by a new line, using putchar
- * Return: Always 0 (Success)
+ * main - Prints base 10 numbers
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints numbers 0-9
+ *
+ * Return: 0 for success
  */
 int main(void)
 {
-	int n;
-	
-	for (n = 48; n < 58; n++)
+	int num = 48;
+
+
+	while (num <= 57)
 	{
-		putchar(n);
+		putchar(num);
+		num++;
 	}
 	putchar('\n');
 	return (0);
