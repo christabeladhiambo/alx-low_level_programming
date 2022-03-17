5c2407a1415a612faa124d6ea0d890d4e348a519
christabel <christabeladhiambo312gmail.com>
2022-03-17 12:50:41 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9,
+ * print_most_numbers prints the numbers, from 0 to 9,
  * except 2 and 4, followed by a new line
  */
 void print_most_numbers(void)
@@ -10,10 +10,10 @@ void print_most_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-	if (i != 2 && i != 4)
-	{
-	_putchar(i + '0');
-	}
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 	}
 
 	_putchar('\n');
