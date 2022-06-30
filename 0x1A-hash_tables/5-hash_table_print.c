ac5c580bf275436074019bcb7a19e35b087ed961
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-06-30 07:31:17 -0700

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,63 +1,32 @@
-nclude "hash_tables.h"
-
-
+#include "hash_tables.h"
 
 /**
-
-  *hash_table_print - Print the hash table as a python directory
-
-  *
-
-  * @ht: Pointer to the hash table
-
-  *
-
-  * Return: Anything, cause void function
-
-  */
-
+*hash_table_print - Print the hash table as a python directory
+*
+* @ht: Pointer to the hash table
+*
+* Return: Anything, cause void function
+*/
 void hash_table_print(const hash_table_t *ht)
-
 {
+	unsigned long int i, j = 0;
+	hash_node_t *head;
 
-		unsigned long int i, j = 0;
+	if (!ht || !ht->array || ht->size == 0)
+		return;
 
-			hash_node_t *head;
-
-
-
-				if (!ht || !ht->array || ht->size == 0)
-
-							return;
-
-
-
-					printf("{");
-
-						for (i = 0; i < ht->size; i++)
-
-								{
-
-											head = ht->array[i];
-
-													while (head)
-
-																{
-
-																				if (j != 0)
-
-																									printf(", ");
-
-																							printf("'%s': '%s'", head->key, head->value);
-
-																										head = head->next;
-
-																													j++;
-
-																															}
-
-														}
-
-							printf("}\n");
-
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		head = ht->array[i];
+		while (head)
+		{
+			if (j != 0)
+				printf(", ");
+			printf("'%s': '%s'", head->key, head->value);
+			head = head->next;
+			j++;
+		}
+	}
+	printf("}\n");
 }
