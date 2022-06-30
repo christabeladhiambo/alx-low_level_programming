0920fe1a80cce32d5b9aba99063538119ec8a2f1
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-06-30 07:12:02 -0700

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,63 +1,32 @@
-nclude "hash_tables.h"
-
-
+#include "hash_tables.h"
 
 /**
-
-  * hash_table_get - Get the content of a key in the hash
-
-  *
-
-  * @ht: The given key
-
-  * @key: The given key to get the value
-
-  *
-
-  * Return: The value
-
-  */
-
+* hash_table_get - Get the content of a key in the hash
+*
+* @ht: The given key
+* @key: The given key to get the value
+*
+* Return: The value
+*/
 char *hash_table_get(const hash_table_t *ht, const char *key)
-
 {
+	unsigned long int index;
+	hash_node_t *head;
 
-		unsigned long int index;
+	if (!ht || !ht->array || ht->size == 0)
+		return (NULL);
 
-			hash_node_t *head;
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
 
+	if (!head)
+		return (NULL);
 
-
-				if (!ht || !ht->array || ht->size == 0)
-
-							return (NULL);
-
-
-
-					index = key_index((const unsigned char *)key, ht->size);
-
-						head = ht->array[index];
-
-
-
-							if (!head)
-
-										return (NULL);
-
-
-
-								while (head)
-
-										{
-
-													if (strcmp(head->key, key) == 0)
-
-																	return (head->value);
-
-															head = head->next;
-
-																}
-
-									return (NULL);
-
+	while (head)
+	{
+		if (strcmp(head->key, key) == 0)
+			return (head->value);
+		head = head->next;
+	}
+	return (NULL);
 }
