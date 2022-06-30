c9e98297a61186145ec8029512227dad57085745
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-06-30 07:34:51 -0700

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,63 +1,32 @@
-nclude "hash_tables.h"
-
-
+#include "hash_tables.h"
 
 /**
-
-  * hash_table_delete - Del the hash table
-
-  *
-
-  * @ht: Pointer to the hash table
-
-  *
-
-  * Return: Anything, cause void function
-
-  */
-
+* hash_table_delete - Del the hash table
+*
+* @ht: Pointer to the hash table
+*
+* Return: Anything, cause void function
+*/
 void hash_table_delete(hash_table_t *ht)
-
 {
+	unsigned long int i;
+	hash_node_t *head, *tmp;
 
-		unsigned long int i;
+	if (!ht || !ht->array || ht->size == 0)
+		return;
 
-			hash_node_t *head, *tmp;
-
-
-
-				if (!ht || !ht->array || ht->size == 0)
-
-							return;
-
-
-
-					for (i = 0; i < ht->size; i++)
-
-							{
-
-										head = ht->array[i];
-
-												while (head)
-
-															{
-
-																			tmp = head;
-
-																						head = head->next;
-
-																									free(tmp->key);
-
-																												free(tmp->value);
-
-																															free(tmp);
-
-																																	}
-
-													}
-
-						free(ht->array);
-
-							free(ht);
-
+	for (i = 0; i < ht->size; i++)
+	{
+		head = ht->array[i];
+		while (head)
+		{
+			tmp = head;
+			head = head->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
