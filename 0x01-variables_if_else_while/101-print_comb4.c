33d71495850424023c43dc0137d20d70a44a8abd
christabel <christabeladhiambo312gmail.com>
2022-09-05 09:22:36 -0700

    Printtc

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
- * Return: Always 0 (Success)
- */
+* main - Print list of number, sorted by a certain sort, without using printf
+* Return: 0 void function
+*/
 int main(void)
 {
-	int n, m, l;
+	int i, j, k;
 
-	for (n = 48; n < 58; n++)
-        {
-	       for (m = 49; m < 58; m++)
-	       {
-	              for (l = 50; l < 58; l++)
-	              {
-	                      if (l > m && m > n)
-		              {
-		                      putchar(n);
-		                      putchar(m);
-			              putchar(l);
-			              if (n != 55 || m != 56)
-				      {
-				             putchar(',');
-			                     putchar(' ');
-			              }
-		 	       }
-		      }
-	       }
+	i = 0;
+	while (i < 10)
+	{
+		j = i + 1;
+		while (j < 10)
+		{
+			k = j + 1;
+			while (k < 10)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				k++;
+				if (i != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		i++;
 	}
-}	
+	putchar('\n');
+return (0);
+}
